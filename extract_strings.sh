f863f62f89000330c38d898cb9d19468575d759f
ha_60 <hadi60@gmail.com>
2010-05-17 16:57:13 +0430

    fix Translation typo and update template

--- extract_strings.sh
+++ extract_strings.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xgettext -k_ -kN_ -o po/messages.pot src/*.py data/*.glade
+xgettext -k_ -kN_ -o po/messages.pot amir/*.py bin/amir data/ui/*.glade
 
 /usr/lib/pymodules/python2.6/formencode/i18n/msgfmt.py po/*.po 
