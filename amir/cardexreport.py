56923ed1758cb843454121789268dcaa2e3cd35b
Daniyal Javani Joni <daniyal.javani@gmail.com>
2018-02-28 17:14:28 +0330

    Add filter for factors of cardex

--- amir/cardexreport.py
+++ amir/cardexreport.py
@@ -25,7 +25,7 @@ class CardexReport:
         self.window = self.builder.get_object("window1")
         
         self.treeview = self.builder.get_object("tradeTreeView")
-        self.treestore = Gtk.TreeStore(str, str, str, str, str, str)
+        self.treestore = Gtk.TreeStore(str, str, str, str, str, str, str)
         self.treeview.set_model(self.treestore)
 
         column = Gtk.TreeViewColumn(_("Factor Code"), Gtk.CellRendererText(), text = 0)
@@ -35,35 +35,42 @@ class CardexReport:
         column.set_sort_indicator(True)
         self.treeview.append_column(column)
 
-        column = Gtk.TreeViewColumn(_("Customer Name"), Gtk.CellRendererText(), text = 1)
+        column = Gtk.TreeViewColumn(_("Customer Code"), Gtk.CellRendererText(), text = 1)
+        column.set_spacing(5)
+        column.set_resizable(True)
+        column.set_sort_column_id(1)
+        column.set_sort_indicator(True)
+        self.treeview.append_column(column)
+
+        column = Gtk.TreeViewColumn(_("Customer Name"), Gtk.CellRendererText(), text = 2)
         column.set_spacing(5)
         column.set_resizable(True)
         column.set_sort_column_id(1)
         column.set_sort_indicator(True)
         self.treeview.append_column(column)
         
-        column = Gtk.TreeViewColumn(_("Sell Quantity"), Gtk.CellRendererText(), text = 2)
+        column = Gtk.TreeViewColumn(_("Sell Quantity"), Gtk.CellRendererText(), text = 3)
         column.set_spacing(5)
         column.set_resizable(True)
         column.set_sort_column_id(2)
         column.set_sort_indicator(True)
         self.treeview.append_column(column)
 
-        column = Gtk.TreeViewColumn(_("Buy Quantity"), Gtk.CellRendererText(), text = 3)
+        column = Gtk.TreeViewColumn(_("Buy Quantity"), Gtk.CellRendererText(), text = 4)
         column.set_spacing(5)
         column.set_resizable(True)
         column.set_sort_column_id(3)
         column.set_sort_indicator(True)
         self.treeview.append_column(column)
         
-        column = Gtk.TreeViewColumn(_("Total Price"), Gtk.CellRendererText(), text = 4)
+        column = Gtk.TreeViewColumn(_("Total Price"), Gtk.CellRendererText(), text = 5)
         column.set_spacing(5)
         column.set_resizable(True)
         column.set_sort_column_id(4)
         column.set_sort_indicator(True)
         self.treeview.append_column(column)
 
-        column = Gtk.TreeViewColumn(_("Date"), Gtk.CellRendererText(), text = 5)
+        column = Gtk.TreeViewColumn(_("Date"), Gtk.CellRendererText(), text = 6)
         column.set_spacing(5)
         column.set_resizable(True)
         column.set_sort_column_id(5)
@@ -76,14 +83,10 @@ class CardexReport:
         self.window.show_all()
         self.builder.connect_signals(self)
 
-    def searchProduct(self,sender):
-        # Get data from DB
-        box = self.builder.get_object("productCodeSearchEntry")
-        text = box.get_text()
+    def show(self, productCode,factorType,customerCode,dateFrom,dateTo):
         query = config.db.session.query(Products)
-        query = query.filter(Products.code == text)
+        query = query.filter(Products.code == productCode)
         bill  = query.first()
-
         if bill is not None:
             box = self.builder.get_object("statusTextView").get_buffer().set_text('')
             box = self.builder.get_object("nameTextView").get_buffer().set_text(bill.name)
@@ -102,6 +105,16 @@ class CardexReport:
             #Fill factor treeview
             query = config.db.session.query(Exchanges,Trades,Customers)
             query = query.filter(bill.id == Exchanges.exchngProduct, Exchanges.exchngTransId == Trades.Id, Customers.custId == Trades.Cust)
+            if factorType:
+                if factorType == 'Sell':
+                    factorType = 1
+                else:
+                    factorType = 0
+                query = query.filter(Trades.Sell == factorType)
+
+            if customerCode:
+                query = query.filter(Customers.custCode == customerCode)
+
             result = query.all()
             
             for factor in result:
@@ -111,8 +124,32 @@ class CardexReport:
                 else:
                     buy_quantity = str(factor.Exchanges.exchngQnty)
                     sell_quantity = '-'
-                self.treestore.append(None, (str(factor.Trades.Code), str(factor.Customers.custName), str(sell_quantity), str(buy_quantity),
+                self.treestore.append(None, (str(factor.Trades.Code), str(factor.Customers.custCode), str(factor.Customers.custName), str(sell_quantity), str(buy_quantity),
                  str(factor.Exchanges.exchngQnty * factor.Exchanges.exchngUntPrc), str(factor.Trades.tDate)))
         else:
             box = self.builder.get_object("statusTextView").get_buffer().set_text("Not Found")
         self.window.show_all()
+
+    def searchProduct(self,sender):
+        # Get data from DB
+        box = self.builder.get_object("productCodeSearchEntry")
+        productCode = box.get_text()
+        self.show(productCode,0,0,0,0)
+        
+
+    def factorFilter(self,sender):
+        combo = self.builder.get_object("typeComboBox")
+        index = combo.get_active()
+        model = combo.get_model()
+        item = model[index]
+
+        box = self.builder.get_object("productCodeSearchEntry")
+        productCode = box.get_text()
+
+        box = self.builder.get_object("customerCodeEntry")
+        customerCode = box.get_text()
+        if index == -1:
+            productType = None
+        else:
+            productType = item[0]
+        self.show(productCode,productType,customerCode,0,0)
