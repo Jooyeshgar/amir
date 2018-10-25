f3b3373da272ac0726ae992b9ffcb7e97b9de03a
nazaninIT <jouanii@gmail.com>
2018-10-25 16:02:25 +0330

    enhanced cardex report  searching...

--- amir/cardexreport.py
+++ amir/cardexreport.py
@@ -22,10 +22,10 @@ class CardexReport:
         self.window = self.builder.get_object("window1")
         
         self.treeview = self.builder.get_object("factorTreeView")
-        self.treestore = Gtk.TreeStore(str, str, str, str, str, str, str,str)
+        self.treestore = Gtk.TreeStore(str, str, str, str, str, str, str,str , str)
         self.treeview.set_model(self.treestore)
 
-        headers = (_("Date"),_("Type"),_("Quantity"), _("Unit Price"),_("Total Price"),_("Remained"), _("Factor Code") , _("Customer Name") ) 
+        headers = (_("Date"),_("Type"),_("Quantity"), _("Unit Price"),_("Total Price"),_("Remained"), _("Factor Code") , _("Customer Name"),_("Customer Code") ) 
         #headers = (_("Factor Code") ,_("Customer Code"), _("Customer Name"),_("Stock inventory"),_("Sell Quantity"),_("Buy Quantity") , _("Total Price"),_("Date") ) 
         i = 0
         for header in headers :
@@ -36,7 +36,7 @@ class CardexReport:
             column.set_sort_indicator(True)
             self.treeview.append_column(column)
             i+=1
-        
+        self.rows = []
         self.window.show_all()
         self.builder.connect_signals(self)
         self.session = config.db.session  
@@ -93,8 +93,7 @@ class CardexReport:
         query = config.db.session.query(Products)
         query = query.filter(Products.code == utility.convertToLatin(productCode) )
         product  = query.first()
-        self.treestore.clear()
-        rows = []
+        self.treestore.clear()        
         if product is not None:
             statusbar = self.builder.get_object('statusbar3')
             context_id = statusbar.get_context_id('statusbar')
@@ -114,37 +113,35 @@ class CardexReport:
             totalSellQnt = totalBuyQnt = totalSellIncome = totalBuyPrice = 0
             #Fill factor treeview
             initQ = config.db.session.query(FactorItems).filter(FactorItems.factorId == 0).first()
+            productCount = 0 
             if initQ:
                 totalPrice = float(initQ.untPrc) * float(initQ.qnty)
-                self.treestore.append(None, ("-" ,_("Initial inventory") ,utility.LN( initQ.qnty),utility.LN(initQ.untPrc),utility.LN(totalPrice) ,utility.LN(initQ.qnty),  "-" , "-" ) ) 
+                self.rows.append(("-" ,_("Initial inventory") ,utility.LN( initQ.qnty),utility.LN(initQ.untPrc),utility.LN(totalPrice) ,utility.LN(initQ.qnty),  "-" , "-" ,"-") )
                 totalBuyPrice += float(initQ.untPrc) * float(initQ.qnty)                
                 totalBuyQnt += float(initQ.qnty)
+                productCount = initQ.qnty
             query = config.db.session.query(FactorItems,Factors,Customers)
             query = query.filter(product.id == FactorItems.productId, FactorItems.factorId == Factors.Id, Customers.custId == Factors.Cust)            
-            if factorType>= 0 and factorType != 2:
-                query = query.filter(Factors.Sell == factorType)
+            # if factorType>= 0 and factorType != 2:
+            #     query = query.filter(Factors.Sell == factorType)
 
-            if customerCode:
-                query = query.filter(Customers.custCode == utility.convertToLatin(customerCode) )
+            # if customerCode:
+            #     query = query.filter(Customers.custCode == utility.convertToLatin(customerCode) )
 
-            if dateTo:
-                dateTo = stringToDate(dateTo)
+            # if dateTo:
+            #     dateTo = stringToDate(dateTo)
 
-            if dateFrom:
-                dateFrom = stringToDate(dateFrom)
+            # if dateFrom:
+            #     dateFrom = stringToDate(dateFrom)
 
-            if dateTo:
-                query = query.filter(Factors.tDate <= dateTo)
-            if dateFrom:   
-                query = query.filter(Factors.tDate >= dateFrom)
-                
-            query = query.order_by(FactorItems.id.desc())
+            # if dateTo:
+            #     query = query.filter(Factors.tDate <= dateTo)
+            # if dateFrom:   
+            #     query = query.filter(Factors.tDate >= dateFrom)
+                           
             result = query.all()
             if result == None or  not len(result):
                 return 
-            proQuan = config.db.session.query(Products.quantity).filter(Products.id == (result[0]).FactorItems.productId).first()
-            if proQuan:
-                productCount = float(proQuan.quantity)
 
             for factor in result:                
                 quantity = utility.LN(factor.FactorItems.qnty)
@@ -152,20 +149,19 @@ class CardexReport:
                 totalPrice = utility.LN(factor.FactorItems.untPrc * factor.FactorItems.qnty)
                 if factor.Factors.Sell == True:
                     ftype = _("Sell")
-                    productChange = float(factor.FactorItems.qnty)                                    
+                    productCount -= float(factor.FactorItems.qnty)                                    
                     totalSellIncome += float(factor.FactorItems.qnty) * float(factor. FactorItems.untPrc)    # kole foroosh = sum(tedad * gheymat)
                     totalSellQnt += float(factor.FactorItems.qnty)                            # kole tedade forush = sum (tedad)
                 else:
                     ftype = _("Buy")
-                    productChange = -float(factor.FactorItems.qnty)
+                    productCount += float(factor.FactorItems.qnty)
                     totalBuyPrice += float(factor.FactorItems.qnty) * float(factor. FactorItems.untPrc)   # kole kharid = sum(tedad * gheymat)                    
                     totalBuyQnt += float(factor.FactorItems.qnty) 
 
                 date = dateToString(factor.Factors.tDate)
-                rows.append((date,ftype,quantity,unitPrice, totalPrice ,utility.LN(productCount), utility.readNumber(factor.Factors.Code), unicode(factor.Customers.custName)))                
-                productCount += productChange
-            rows = rows[::-1]
-            for row in rows:
+                self.rows.append((date,ftype,quantity,unitPrice, totalPrice ,utility.LN(productCount), utility.readNumber(factor.Factors.Code), unicode(factor.Customers.custName), unicode(factor.Customers.custCode)))                
+
+            for row in self.rows:
                 self.treestore.append(None,  row)
             profit = totalSellIncome - ((totalBuyPrice/totalBuyQnt) *totalSellQnt )
             profit = "{0:.3f}".format(profit)           # rounding floating point number
@@ -186,12 +182,28 @@ class CardexReport:
         
 
     def factorFilter(self,sender):
+        self.treestore.clear()
         combo = self.builder.get_object("typeComboBox")
         index = combo.get_active()
-        productCode = self.builder.get_object("productCodeSearchEntry").get_text()        
-        customerCode = self.builder.get_object("customerCodeEntry").get_text()
-        delimiter = config.datedelims[config.datedelim] 
-        dateTo = self.dateToEntry.get_text()        
-        dateFrom = self.dateFromEntry.get_text()        
+        productCode = self.builder.get_object("productCodeSearchEntry").get_text()   
 
-        self.showResult(productCode,index,customerCode,dateFrom,dateTo)
+        customerCode =utility.readNumber(self.builder.get_object("customerCodeEntry").get_text())
+        
+        dateTo = self.dateToEntry.get_text()        
+        dateFrom = self.dateFromEntry.get_text()   
+
+        fTypes = [_("Buy") , _("Sell")]        
+        flag = True
+        for row in self.rows:
+            if customerCode and  row[8] != customerCode:
+                flag = False
+            if dateTo and row[0] > dateTo:
+                flag = False
+            if dateFrom and row[0] < dateFrom:
+                flag = False
+
+            if index!=2 and  row[1] != fTypes[index]:            
+                flag = False
+            if flag:                
+                self.treestore.append(None , row)
+            flag = True       
